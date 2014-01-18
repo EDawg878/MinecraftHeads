297fe2d39c3ea94342d443f770922bc37b44ae66
EDawg878 <EDawg878@gmail.com>
2014-01-17 19:45:59 -0800

    Version 1.06

--- src/main/java/com/edawg878/minecraftheads/MinecraftHeads.java
+++ src/main/java/com/edawg878/minecraftheads/MinecraftHeads.java
@@ -1,46 +1,62 @@
 package com.edawg878.minecraftheads;
 
 import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import net.minecraft.util.org.apache.commons.io.FileUtils;
+import net.minecraft.util.org.apache.commons.lang3.text.WordUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinecraftHeads extends JavaPlugin {
 
-    private MinecraftHeads instance;
+    private static final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9_]{2,16}");
     private static final String FILE_NAME = "minecraftheads.csv";
-    private URL url;
+    private static final String DEFAULT_URL = "https://docs.google.com/spreadsheet/pub?key=0AtAZ7aOy1ZWQdE40TUhtNHU1NmstR1VKUlo0aEZleEE&output=csv";
+    private static final int TIMEOUT = 5000;
     private File file;
-    private Map<String, Map<String, String>> heads;
+    private Map<String, Head> heads;
+    private Map<String, List<Head>> categories;
+    private List<String> categoryList;
 
     @Override
     public void onEnable() {
-        instance = this;
         setupConfig();
-        setupURL();
         file = new File(getDataFolder(), FILE_NAME);
         if (!file.exists()) {
             saveResource(FILE_NAME, false);
         }
-        loadFromFile(file);
+        reload();
+        if (heads == null || categories == null || categoryList == null) {
+            getLogger().severe("Error loading heads, disabling plugin...");
+            setEnabled(false);
+        }
         getCommand("mcheads").setExecutor(new MinecraftHeadsCommand(this));
     }
 
-    public void download(final CommandSender sender, final Callback callback) {
+    public void download(CommandSender sender, final Callback callback) {
         getServer().getScheduler().runTaskAsynchronously(this, new Runnable() {
-            @Override
             public void run() {
                 try {
-                    FileUtils.copyURLToFile(url, file, 5000, 5000);
-                    loadFromFile(file);
+                    URL url = getSpreadsheetURL();
+                    FileUtils.copyURLToFile(url, file, TIMEOUT, TIMEOUT);
+                    reload();
                     callback.onCompletion();
                 } catch (IOException ex) {
                     getLogger().severe("Error saving downloaded data to file");
@@ -50,66 +66,204 @@ public class MinecraftHeads extends JavaPlugin {
         });
     }
 
-    public interface Callback {
-
-        void onCompletion();
-
-        void onFailure();
+    public void reload() {
+        loadFromFile(file);
     }
 
-    public Map<String, Map<String, String>> getHeads() {
-        return heads;
+    public Head getHead(String key) {
+        return heads.get(key.toLowerCase());
     }
 
-    public MinecraftHeads getInstance() {
-        return instance;
-    }
-
-    private void setupURL() {
-        try {
-            url = new URL(getConfig().getString("spreadsheet-url"));
-        } catch (MalformedURLException ex) {
-            getLogger().severe("Error: Invalid URL");
-            setEnabled(false);
+    public List<String> getHeadCompletions(String arg) {
+        List<String> completions = new ArrayList<String>();
+        for (String key : heads.keySet()) {
+            if (key.startsWith(arg)) {
+                completions.add(key);
+            }
         }
+        Collections.sort(completions, String.CASE_INSENSITIVE_ORDER);
+        return completions;
+    }
+
+    public List<String> getCategoryCompletions(String arg) {
+        List<String> completions = new ArrayList<String>();
+        for (String key : categories.keySet()) {
+            if (key.startsWith(arg)) {
+                completions.add(key);
+            }
+        }
+        Collections.sort(completions, String.CASE_INSENSITIVE_ORDER);
+        return completions;
+    }
+
+    public List<Head> getHeads(String category) {
+        return categories.get(category.toLowerCase());
+    }
+
+    public List<String> getCategories() {
+        return categoryList;
+    }
+
+    public boolean isCategory(String category) {
+        return categories.containsKey(category.toLowerCase());
+    }
+
+    public boolean isHead(String key) {
+        return heads.containsKey(key.toLowerCase());
     }
 
     private void setupConfig() {
-        getConfig().addDefault("spreadsheet-url", "https://docs.google.com/spreadsheet/pub?key=0AtAZ7aOy1ZWQdE40TUhtNHU1NmstR1VKUlo0aEZleEE&output=csv");
+        getConfig().addDefault("spreadsheet-url", DEFAULT_URL);
         getConfig().options().copyDefaults(false);
         saveConfig();
     }
 
-    private void loadFromFile(File file) {
+    private URL getSpreadsheetURL() throws MalformedURLException {
+        return new URL(getConfig().getString("spreadsheet-url", DEFAULT_URL));
+    }
+
+    public void removeUnsafe() {
         try {
+            File temp = new File(getDataFolder(), "minecraftheads-temporary.csv");
+            if (temp.exists()) {
+                temp.delete();
+            }
+            temp.createNewFile();
             CSVReader reader = new CSVReader(new FileReader(file));
-            String[] categories = reader.readNext();
-            if (categories != null) {
-                heads = new TreeMap<String, Map<String, String>>();
-                for (String category : categories) {
-                    heads.put(format(category.toLowerCase()), new TreeMap<String, String>());
-                }
-                String[] nextLine;
-                while ((nextLine = reader.readNext()) != null) {
-                    int count = 0;
-                    for (String entry : nextLine) {
-                        if (count++ < categories.length) {
-                            String[] split = entry.split(":");
-                            if (split.length == 2) {
-                                heads.get(categories[count-1].toLowerCase()).put(format(split[0]), format(split[1]));
-                            }
-                        }
+            CSVWriter writer = new CSVWriter(new FileWriter(temp));
+            String[] header = reader.readNext();
+            if (header != null) {
+                writer.writeNext(header);
+                String[] line;
+                while ((line = reader.readNext()) != null) {
+                    if (line.length == 6 && line[5].equalsIgnoreCase("yes")) {
+                        writer.writeNext(line);
                     }
                 }
             }
+            writer.close();
+            reader.close();
+            temp.renameTo(file);
         } catch (FileNotFoundException ex) {
             getLogger().severe("Failed to find file to load from");
         } catch (IOException ex) {
             getLogger().severe("Error reading from file");
         }
     }
-    
-    private String format(String word) {
-        return word.replace(" ", "_");
+
+    private void loadFromFile(File file) {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(file));
+            String[] header = reader.readNext();
+            if ((header != null) && (verifyHeader(header))) {
+                heads = new HashMap();
+                categories = new TreeMap();
+                String[] line;
+                Head head;
+                while ((line = reader.readNext()) != null) {
+                    if (line.length == 6) {
+                        String category;
+                        String displayName;
+                        List<String> keys;
+                        String username;
+                        Boolean safe;
+                        if ((category = parseCategory(line[0])) != null
+                                && (displayName = parseDisplayName(line[2])) != null
+                                && (keys = parseKeys(line[3])) != null
+                                && (username = parseUsername(line[4])) != null
+                                && (safe = parseSafe(line[5])) != null) {
+                            String id = parseID(line[1]);
+                            head = new Head(displayName, username, id, safe);
+                            if (categories.get(category) == null) {
+                                categories.put(category, new ArrayList());
+                            }
+                            if (id != null) {
+                                heads.put(id, head);
+                            }
+                            categories.get(category).add(head);
+                            for (String key : keys) {
+                                heads.put(key, head);
+                            }
+                        }
+                    }
+                }
+                categoryList = new ArrayList<String>(categories.keySet());
+            }
+            reader.close();
+        } catch (FileNotFoundException ex) {
+            getLogger().severe("Failed to find file to load from");
+        } catch (IOException ex) {
+            getLogger().severe("Error reading from file");
+        }
+    }
+
+    private boolean verifyHeader(String[] header) {
+        if (header.length == 6) {
+            return true;
+        } else if (header.length == 1 && header[0].equalsIgnoreCase("#DefaultFile")) {
+            getLogger().info("Default heads loaded, to download the latest use /mcheads download");
+            return true;
+        }
+        return false;
+    }
+
+    private String parseCategory(String category) {
+        return category.isEmpty() ? null : category.toLowerCase();
+    }
+
+    private String parseID(String id) {
+        return id.isEmpty() ? null : id;
+    }
+
+    private String parseDisplayName(String displayName) {
+        return WordUtils.capitalizeFully(displayName);
+    }
+
+    private List<String> parseKeys(String keys) {
+        List<String> list = new ArrayList<String>();
+        String[] split = keys.split(",");
+        for (String entry : split) {
+            list.add(entry.toLowerCase().replace(" ", ""));
+        }
+        return list.isEmpty() ? null : list;
+    }
+
+    private String parseUsername(String username) {
+        if (USERNAME_PATTERN.matcher(username).matches()) {
+            return username;
+        }
+        return null;
+    }
+
+    private Boolean parseSafe(String safe) {
+        if (safe.equalsIgnoreCase("yes")) {
+            return true;
+        } else if (safe.equalsIgnoreCase("no")) {
+            return true;
+        }
+        return null;
+    }
+
+    @AllArgsConstructor
+    public class Head {
+
+        @Getter
+        private final String displayName, username, id;
+        private final boolean safe;
+
+        public boolean isSafe() {
+            return safe;
+        }
+
+        public String getSafe() {
+            return ChatColor.RED + "UNSAFE";
+        }
+    }
+
+    public interface Callback {
+
+        public void onCompletion();
+
+        public void onFailure();
     }
 }
--- src/main/java/com/edawg878/minecraftheads/MinecraftHeadsCommand.java
+++ src/main/java/com/edawg878/minecraftheads/MinecraftHeadsCommand.java
@@ -1,81 +1,87 @@
 package com.edawg878.minecraftheads;
 
 import com.edawg878.minecraftheads.MinecraftHeads.Callback;
+import com.edawg878.minecraftheads.MinecraftHeads.Head;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
 import net.minecraft.util.org.apache.commons.lang3.math.NumberUtils;
-import org.apache.commons.lang.WordUtils;
+import net.minecraft.util.org.apache.commons.lang3.text.WordUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
-/**
- *
- * @author EDawg878 <EDawg878@gmail.com>
- */
-public class MinecraftHeadsCommand implements CommandExecutor {
+public class MinecraftHeadsCommand implements CommandExecutor, TabCompleter {
 
     private final MinecraftHeads plugin;
 
     public MinecraftHeadsCommand(MinecraftHeads instance) {
-        plugin = instance;
+        this.plugin = instance;
     }
 
-    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1 && sender.isOp() || sender.hasPermission("mcheads.get")) {
+            return plugin.getHeadCompletions(args[0].toLowerCase());
+        }
+        if (args.length == 2 && args[0].equalsIgnoreCase("list") && sender.isOp() || sender.hasPermission("mcheads.list")) {
+            return plugin.getCategoryCompletions(args[1].toLowerCase());
+        }
+        return null;
+    }
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 1) {
+        if (args.length == 0) {
+            getPlayerHead(sender, sender.getName(), 1);
+            return true;
+        } else if (args.length == 1) {
             if (args[0].equalsIgnoreCase("list")) {
                 showCategories(sender);
-                return true;
             } else if (args[0].equalsIgnoreCase("download")) {
                 download(sender);
-                return true;
+            } else if (args[0].equalsIgnoreCase("help")) {
+                usage(sender);
+            } else if (args[0].equalsIgnoreCase("reload")) {
+                reload(sender);
+            } else if (args[0].equalsIgnoreCase("removeunsafe")) {
+                removeUnsafe(sender);
+            } else {
+                getHead(sender, args[0], 1);
             }
-            return false;
+            return true;
         } else if (args.length == 2) {
             if (args[0].equalsIgnoreCase("list")) {
                 showAvailableHeads(sender, args[1], 1);
                 return true;
             }
-            return false;
-        } else if (args.length == 3) {
-            if (args[0].equalsIgnoreCase("list")) {
-                if (StringUtils.isNumeric(args[2])) {
-                    int page = NumberUtils.toInt(args[2]);
-                    showAvailableHeads(sender, args[1], page);
-                    return true;
-                }
-            } else if (args[0].equalsIgnoreCase("get")) {
-                if (StringUtils.isNumeric(args[2])) {
-                    int number = NumberUtils.toInt(args[2]);
-                    getHead(sender, args[1], number, 1);
-                } else {
-                    getHead(sender, args[1], args[2], 1);
-                }
+            if (args[0].equalsIgnoreCase("player")) {
+                getPlayerHead(sender, args[1], 1);
                 return true;
             }
-        } else if (args.length == 4) {
-            if (args[0].equalsIgnoreCase("get")) {
-                if (StringUtils.isNumeric(args[3])) {
-                    int quantity = NumberUtils.toInt(args[3]);
-                    if (StringUtils.isNumeric(args[2])) {
-                        int number = NumberUtils.toInt(args[2]);
-                        getHead(sender, args[1], number, quantity);
-                    } else {
-                        getHead(sender, args[1], args[2], quantity);
-                    }
-                    return true;
-                }
+            if (StringUtils.isNumeric(args[1])) {
+                int quantity = NumberUtils.toInt(args[1]);
+                getHead(sender, args[0], quantity);
+                return true;
+            }
+        } else if ((args.length == 3)
+                && (StringUtils.isNumeric(args[2]))) {
+            int num = NumberUtils.toInt(args[2]);
+            if (args[0].equalsIgnoreCase("player")) {
+                getPlayerHead(sender, args[1], num);
+                return true;
+            }
+            if (args[0].equalsIgnoreCase("list")) {
+                showAvailableHeads(sender, args[1], num);
+                return true;
             }
         }
         invalidArgument(sender);
+        usage(sender);
         return false;
     }
 
@@ -83,16 +89,18 @@ public class MinecraftHeadsCommand implements CommandExecutor {
         sender.sendMessage(ChatColor.RED + "Invalid Argument");
     }
 
-    private void notifySpawnedHeads(Player player, String displayName, int amount) {
-        String head = amount == 1? "head" : "heads";
-        player.sendMessage(ChatColor.GOLD + "Spawned " + ChatColor.RED + amount + ChatColor.GOLD + " " + WordUtils.capitalize(displayName) + " " + head);
+    private void usage(CommandSender sender) {
+        sender.sendMessage(ChatColor.GOLD + "/mcheads list - List categories");
+        sender.sendMessage(ChatColor.GOLD + "/mcheads list <category> - List heads in category");
+        sender.sendMessage(ChatColor.GOLD + "/mcheads <key> [qty] - Spawn heads by name or id");
+        sender.sendMessage(ChatColor.GOLD + "/mcheads [player] [qty] - Spawn player heads");
     }
 
     private void showCategories(CommandSender sender) {
         if (sender.isOp() || sender.hasPermission("mcheads.list")) {
             sender.sendMessage(ChatColor.GOLD + "Available Categories: ");
-            for (String category : plugin.getHeads().keySet()) {
-                sender.sendMessage(ChatColor.RED + "- " + ChatColor.GOLD + WordUtils.capitalize(category));
+            for (String category : plugin.getCategories()) {
+                sender.sendMessage(ChatColor.RED + "- " + ChatColor.GOLD + WordUtils.capitalizeFully(category));
             }
         } else {
             sender.sendMessage(ChatColor.RED + "No permission");
@@ -101,18 +109,19 @@ public class MinecraftHeadsCommand implements CommandExecutor {
 
     private void showAvailableHeads(CommandSender sender, String category, int page) {
         if (sender.isOp() || sender.hasPermission("mcheads.list")) {
-            category = category.toLowerCase();
-            if (plugin.getHeads().containsKey(category)) {
-                Map<String, String> heads = plugin.getHeads().get(category);
-                List<String> keys = new ArrayList<String>(heads.keySet());
+            if (plugin.isCategory(category)) {
+                List<Head> heads = plugin.getHeads(category);
                 final int pagesize = 8;
-                final int maxpage = (int) Math.ceil(heads.size() / (double) pagesize);
+                int maxpage = (int) Math.ceil(heads.size() / pagesize);
                 if (page > 0 && page <= maxpage) {
-                    sender.sendMessage(ChatColor.GOLD + "Available Heads: (page " + page + "/" + maxpage + ")");
-                    for (int i = (page - 1) * pagesize; (i < heads.size()) && (i < page * pagesize); i++) {
-                        String displayName = keys.get(i);
-                        String username = heads.get(displayName);
-                        sender.sendMessage(ChatColor.GOLD + "[" + (i + 1) + "] " + ChatColor.RED + displayName + ChatColor.GOLD + " [" + username + "]");
+                    sender.sendMessage(ChatColor.GOLD + "Heads in Category " + category + ": (page " + page + "/" + maxpage + ")");
+                    for (int i = (page - 1) * pagesize; i < heads.size() && i < page * pagesize; i++) {
+                        Head head = heads.get(i);
+                        if (head.getId() != null) {
+                            sender.sendMessage(ChatColor.GOLD + "[" + head.getId() + "] " + ChatColor.GOLD + head.getDisplayName() + " [" + head.getSafe() + ChatColor.GOLD + "]");
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "- " + ChatColor.GOLD + head.getDisplayName() + " [" + head.getSafe() + ChatColor.GOLD + "]");
+                        }
                     }
                 } else {
                     sender.sendMessage(ChatColor.RED + "Invalid page");
@@ -125,50 +134,14 @@ public class MinecraftHeadsCommand implements CommandExecutor {
         }
     }
 
-    private void getHead(CommandSender sender, String category, int number, int quantity) {
+    private void getHead(CommandSender sender, String key, int quantity) {
         if (sender.isOp() || sender.hasPermission("mcheads.get")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                category = category.toLowerCase();
-                if (plugin.getHeads().containsKey(category)) {
-                    Map<String, String> heads = plugin.getHeads().get(category);
-                    List<String> keys = new ArrayList<String>(heads.keySet());
-                    if (number > 0 && number <= keys.size()) {
-                        String displayName = keys.get(number-1);
-                        String username = heads.get(displayName);
-                        spawnHead(player, username, quantity);
-                        notifySpawnedHeads(player, displayName, quantity);
-                    } else {
-                        sender.sendMessage(ChatColor.RED + "Invalid head number");
-                    }
+                if (plugin.isHead(key)) {
+                    spawnHead(player, plugin.getHead(key), quantity);
                 } else {
-                    sender.sendMessage(ChatColor.RED + "Invalid Category");
-                }
-            } else {
-                sender.sendMessage(ChatColor.RED + "You must be in-game to execute this command");
-            }
-        } else {
-            sender.sendMessage(ChatColor.RED + "No permission");
-        }
-    }
-
-    private void getHead(CommandSender sender, String category, String displayName, int quantity) {
-        if (sender.isOp() || sender.hasPermission("mcheads.get")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                category = category.toLowerCase();
-                displayName = WordUtils.capitalize(displayName);
-                if (plugin.getHeads().containsKey(category)) {
-                    Map<String, String> heads = plugin.getHeads().get(category);
-                    if (heads.containsKey(displayName)) {
-                        String username = heads.get(displayName);
-                        spawnHead(player, username, quantity);
-                        notifySpawnedHeads(player, displayName, quantity);
-                    } else {
-                        sender.sendMessage(ChatColor.RED + "Invalid head name");
-                    }
-                } else {
-                    sender.sendMessage(ChatColor.RED + "Invalid Category");
+                    sender.sendMessage(ChatColor.RED + "Invalid head name");
                 }
             } else {
                 sender.sendMessage(ChatColor.RED + "You must be in-game to execute this command");
@@ -182,28 +155,80 @@ public class MinecraftHeadsCommand implements CommandExecutor {
         if (sender.isOp() || sender.hasPermission("mcheads.download")) {
             sender.sendMessage(ChatColor.GOLD + "Downloading latest heads...");
             plugin.download(sender, new Callback() {
-
+                @Override
                 public void onCompletion() {
                     sender.sendMessage(ChatColor.GOLD + "File download complete");
                 }
 
+                @Override
                 public void onFailure() {
                     sender.sendMessage(ChatColor.RED + "Error downloading file");
                 }
-
             });
         } else {
             sender.sendMessage(ChatColor.RED + "No permission");
         }
     }
 
-    private void spawnHead(Player player, String targetName, int amount) {
+    private void spawnHead(Player player, Head head, int amount) {
         ItemStack skull = new ItemStack(Material.SKULL_ITEM, amount);
         skull.setDurability((short) 3);
         SkullMeta meta = (SkullMeta) skull.getItemMeta();
-        meta.setOwner(targetName);
+        meta.setOwner(head.getUsername());
+        meta.setDisplayName(ChatColor.BLUE + head.getDisplayName());
+        List<String> lore = new ArrayList<String>();
+        lore.add(ChatColor.GOLD + "Username: " + ChatColor.RED + head.getUsername());
+        lore.add(ChatColor.GOLD + "Safe: " + ChatColor.RED + WordUtils.capitalizeFully(String.valueOf(head.isSafe())));
+        lore.add(ChatColor.GOLD + "Generated from " + ChatColor.RED + "MinecraftHeads" + ChatColor.GOLD + " by " + ChatColor.RED + "EDawg878");
+        meta.setLore(lore);
         skull.setItemMeta(meta);
         player.getInventory().addItem(skull);
+        String heads = amount == 1 ? "head" : "heads";
+        player.sendMessage(ChatColor.GOLD + "Spawned " + ChatColor.RED + amount + ChatColor.GOLD + " " + ChatColor.ITALIC + head.getDisplayName() + ChatColor.GOLD + " " + heads + " [" + head.getSafe() + ChatColor.GOLD + "]");
     }
 
+    private void getPlayerHead(CommandSender sender, String target, int amount) {
+        if (sender.isOp() || sender.hasPermission("mcheads.get")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                ItemStack skull = new ItemStack(Material.SKULL_ITEM, amount);
+                skull.setDurability((short) 3);
+                SkullMeta meta = (SkullMeta) skull.getItemMeta();
+                meta.setOwner(target);
+                meta.setDisplayName(ChatColor.BLUE + target);
+                List<String> lore = new ArrayList<String>();
+                lore.add(ChatColor.GOLD + "Username: " + ChatColor.RED + target);
+                lore.add(ChatColor.GOLD + "Generated from " + ChatColor.RED + "MinecraftHeads" + ChatColor.GOLD + " by " + ChatColor.RED + "EDawg878");
+                meta.setLore(lore);
+                skull.setItemMeta(meta);
+                player.getInventory().addItem(skull);
+                String heads = amount == 1 ? "head" : "heads";
+                player.sendMessage(ChatColor.GOLD + "Spawned " + ChatColor.RED + amount + ChatColor.GOLD + ChatColor.ITALIC + " Player " + ChatColor.GOLD + heads + " [" + ChatColor.RED + target + ChatColor.GOLD + "]");
+            } else {
+                sender.sendMessage(ChatColor.RED + "You must be in-game to execute this command");
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "No permission");
+        }
+    }
+
+    private void reload(CommandSender sender) {
+        if (sender.isOp() || sender.hasPermission("mcheads.reload")) {
+            plugin.reloadConfig();
+            plugin.reload();
+            sender.sendMessage(ChatColor.GOLD + "MinecraftHeads reloaded");
+        } else {
+            sender.sendMessage(ChatColor.RED + "No permission");
+        }
+    }
+
+    private void removeUnsafe(CommandSender sender) {
+        if (sender.isOp() || sender.hasPermission("mcheads.removeunsafe")) {
+            plugin.removeUnsafe();
+            plugin.reload();
+            sender.sendMessage(ChatColor.GOLD + "MinecraftHeads removed All unsafe heads");
+        } else {
+            sender.sendMessage(ChatColor.RED + "No permission");
+        }
+    }
 }
